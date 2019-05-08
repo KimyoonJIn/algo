e73b696ad8f487b85743ad60973f21253ba12b36
yoonjin <yoonjin@icon.foundation>
2019-05-08 15:36:44 +0900

    Upload my solution of the newclustering problem

---
+++ KAKAO/newsClustering/solution.py
@@ -0,0 +1,51 @@
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+import re
+from collections import Counter
+
+def create_data_set(input):
+    result = []
+    for i in range(len(input)):
+        text = re.sub('[^a-zA-Z]+', '', input[i:i+2])
+        temp = text.replace(" ", "")
+        if len(temp) == 2:
+            result.append(temp.upper())             
+    return Counter(result)
+
+
+def solution(str1, str2):
+
+    set1 = create_data_set(str1)
+    set2 = create_data_set(str2)
+
+    inter = sum((set1& set2).values())
+    union = sum((set1|set2).values())
+
+    if inter == 0 and union == 0:
+        return 65536
+    else:
+        return int((inter/union)*65536)
---
+++ KAKAO/newsClustering/solution2.py
@@ -0,0 +1,23 @@
+import re
+from collections import Counter
+    
+def create_data_set(input):
+    result = []
+    for i in range(len(input)):
+        temp = re.sub('[^a-zA-Z]+', '', input[i:i+2])
+        if len(temp) == 2:
+            result.append(temp.upper())             
+    return Counter(result)
+ 
+
+def solution(str1, str2):
+    set1 = create_data_set(str1)
+    set2 = create_data_set(str2)
+    
+    inter = sum((set1& set2).values())
+    union = sum((set1|set2).values())
+    
+    if inter == 0 and union == 0:
+        return 65536
+    else:
+        return int((inter/union)*65536)
